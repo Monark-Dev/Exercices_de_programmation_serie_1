0f72eb6e92da605d72b44b6d08e6b5f1622d18b8
Monark-Dev <ea.monark.dev@gmail.com>
2021-01-29 10:39:47 -0500

    Exercice10

--- src/Exercice10.java
+++ src/Exercice10.java
@@ -2,11 +2,10 @@ import javax.swing.JOptionPane;
 
 public class Exercice10 {
     public static void main(String[] args) {
-        int quantiteAchetee = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantité achetée?"));
-        if(quantiteAchetee > 5)
-            JOptionPane.showMessageDialog(null, "Coût net de l'achat: " + Double.parseDouble(JOptionPane.showInputDialog(null, "Prix unitaire?")) * quantiteAchetee * 0.9);
+        if(Integer.parseInt(JOptionPane.showInputDialog(null, "Quantité achetée?")) > 5)
+            JOptionPane.showMessageDialog(null, "Coût net de l'achat: " + Double.parseDouble(JOptionPane.showInputDialog(null, "Prix unitaire?")) * Integer.parseInt(JOptionPane.showInputDialog(null, "C'est quoi déjà la quantité achetée?")) * 0.9);
         else
-            JOptionPane.showMessageDialog(null, "Coût net de l'achat: " + Double.parseDouble(JOptionPane.showInputDialog(null, "Prix unitaire?")) * quantiteAchetee);
+            JOptionPane.showMessageDialog(null, "Coût net de l'achat: " + Double.parseDouble(JOptionPane.showInputDialog(null, "Prix unitaire?")) * Integer.parseInt(JOptionPane.showInputDialog(null, "C'est quoi déjà la quantité achetée?")));
 
         System.exit(0);
     }
