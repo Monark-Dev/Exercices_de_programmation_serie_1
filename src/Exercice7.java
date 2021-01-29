1bfd9b7d5340638a2f54831a4373e458e4e8056a
Monark-Dev <ea.monark.dev@gmail.com>
2021-01-29 08:42:07 -0500

    Exercice7

---
+++ src/Exercice7.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Exercice7 {
+    public static void main(String[] args) {
+        int nombre1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Entrez un premier nombre:"));
+        int nombre2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Entrez un deuxième nombre:"));
+        if(nombre1 > nombre2)
+            JOptionPane.showMessageDialog(null, nombre1 + " est le plus grand nombre");
+        else if(nombre1 < nombre2)
+            JOptionPane.showMessageDialog(null, nombre2 + " est le plus grand nombre");
+        else
+            JOptionPane.showMessageDialog(null, "Vous êtes idiot!");
+
+        System.exit(0);  
+    }
+}
