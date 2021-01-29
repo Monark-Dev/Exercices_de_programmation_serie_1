6fc33cba8ab8e35892d227ed1d3f2cd6cc0fc127
Monark-Dev <ea.monark.dev@gmail.com>
2021-01-29 09:34:13 -0500

    Exercice8

---
+++ src/Exercice8.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Exercice8 {
+    public static void main(String[] args) {
+        int noteEtudiant = Integer.parseInt(JOptionPane.showInputDialog(null, "Note de l'étudiant sur 20 ?"));
+        int noteEtudiantSur100 = noteEtudiant * 5;
+        if(noteEtudiantSur100 >= 60) 
+            JOptionPane.showMessageDialog(null, "L'étudiant a obtenu " + noteEtudiantSur100 + "% Bravo!");
+        else
+            JOptionPane.showMessageDialog(null, "L'étudiant a obtenu " + noteEtudiantSur100 + "%");
+
+         System.exit(0);
+    }   
+}
