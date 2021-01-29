2deb6f7cf1aef39a86a051fad87e839547e91c1b
Monark-Dev <ea.monark.dev@gmail.com>
2021-01-29 16:01:32 -0500

    DollarsCanadiens

--- src/Exercice06.java
+++ src/Exercice06.java
@@ -1,11 +1,14 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import javax.swing.JOptionPane;
 
 public class Exercice06 {
     public static void main(String[] args) {
         int distanceEnKm = Integer.parseInt(JOptionPane.showInputDialog("Entrez la distance en KM:"));
         int kmParLitre = Integer.parseInt(JOptionPane.showInputDialog("Entrez le nombre de Km/litre:"));
-        double prixEssence = Double.parseDouble(JOptionPane.showInputDialog("Entrez le prix de l'essence au litre:"));
-        JOptionPane.showMessageDialog(null, "le coût du déplacement pour faire ce voyage est de " + distanceEnKm / kmParLitre * prixEssence + "$");
+        Double prixEssence = Double.parseDouble(JOptionPane.showInputDialog("Entrez le prix de l'essence au litre:"));
+        JOptionPane.showMessageDialog(null, "le coût du déplacement pour faire ce voyage est de " + NumberFormat.getCurrencyInstance(Locale.CANADA_FRENCH).format(distanceEnKm/kmParLitre*prixEssence));
         System.exit(0);
     }
 }
